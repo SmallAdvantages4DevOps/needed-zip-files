f5da699c0fd56a61a4f1fb76086bd93a478ad07e
miguelangellus <miguelangellus13@outlook.com>
2022-04-30 15:39:37 -0300

    download zip files

---
+++ downloads.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd ~/Downloads
+wget https://objectstorage.us-ashburn-1.oraclecloud.com/p/tvk3j_I8Uj0tTvPE2A2JDWCF_TZpmw0vDIiHkD8HKzGYxZsZtWXBfaURIZ4djW1k/n/idqfa2z2mift/b/eventos-thecloudbootcamp/o/ICP2/mission3.zip
+wget https://objectstorage.us-ashburn-1.oraclecloud.com/p/t1RPAY7hbbdaSj9fhLJhYetCsFe5CEKufO56vb1l2_6FVz35PJzIukvTJKhRYKGs/n/idqfa2z2mift/b/eventos-thecloudbootcamp/o/ICP2/mission2.zip
+wget https://objectstorage.us-ashburn-1.oraclecloud.com/p/mE7kdT1O8UppsI0aV6qxwi8Rfm4YAn3A9rIX6Yv1FxE2Rv6mI1W_RrUs2CBYWxAx/n/idqfa2z2mift/b/eventos-thecloudbootcamp/o/ICP2/mission1.zip
